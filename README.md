6f0ec4e2843d693dde2a3a3b3a4f2806d87e7254
OJIMA Ryoji <rojima1@gmail.com>
2017-10-25 12:58:34 +0900

    tuned.

--- README.md
+++ README.md
@@ -11,28 +11,28 @@ $ pip3 install --upgrade tensorflow-gpu keras funcy matplotlib h5py
 ## 訓練
 
 ~~~ bash
-$ python3 -m try_squeeze_net.train
+$ python3 train.py
 ~~~
 
 ## 訓練結果の確認
 
 ~~~ bash
-$ python3 -m try_squeeze_net.check
+$ python3 check.py
 ~~~
 
 ![accuracy](./results/accuracy.png)
 
 ![loss](./results/loss.png)
 
-私が試した結果だと、cifar10の精度はxx.xx%になりました。
+私が試した結果だと、cifar10の精度は91.40%になりました。
 
 # Notes
 
-* ごめんなさい。TensorFlow環境でしか試していません。
-* [https://github.com/nutszebra/squeeze_net](https://github.com/nutszebra/squeeze_net)を参考にして作成しています。私の能力だと、論文だけでは作れませんでした……。
+* ごめんなさい。Python3とTensorFlowの環境でしか試していません。
+* [https://github.com/nutszebra/residual_net](https://github.com/nutszebra/residual_net)と[https://github.com/takedarts/resnetfamily](https://github.com/takedarts/resnetfamily)を参考にして作成しています。
 * 検証には、ImageNetではなくCIFAR-10を使用しました。
 * [[2](https://arxiv.org/abs/1603.05027)]のバッチ・ノーマライゼーション→ReLU→畳込みの順序を使用しました。
-* Kerasに関数型プログラミングのテクニックを適用する方法は、[Kerasと関数型プログラミングで、楽ちんに深層学習してみた](https://tail-island.github.io/programming/2017/10/13/keras-and-fp.html)にまとめました。
+* Kerasに関数型プログラミングのテクニックを適用する方法は、[Kerasと関数型プログラミングを使えば、深層学習（ディープ・ラーニング）は楽ちんですよ](https://tail-island.github.io/programming/2017/10/25/keras-and-fp.html)にまとめました。
 
 # References
 
--- train.py
+++ train.py
@@ -1,76 +1,105 @@
+# Implementation of SqueezeNet
+#
+# SqueezeNet: AlexNet-level accuracy with 50x fewer parameters and <0.5MB model size
+# https://arxiv.org/abs/1602.07360
+#
+# Identity Mappings in Deep Residual Networks
+# https://arxiv.org/abs/1603.05027)# Wide Residual Network
+
+
 import numpy as np
 import pickle
 
 from data_set                  import load_data
 from funcy                     import concat, identity, juxt, partial, rcompose, repeat, take
 from keras.callbacks           import LearningRateScheduler
-from keras.layers              import Activation, Add, BatchNormalization, Concatenate, Conv2D, Dropout, GlobalAveragePooling2D, Input, MaxPooling2D
+from keras.layers              import Activation, Add, BatchNormalization, Concatenate, Conv2D, Dense, Dropout, GlobalAveragePooling2D, Input, MaxPooling2D
 from keras.models              import Model, save_model
 from keras.optimizers          import SGD
 from keras.preprocessing.image import ImageDataGenerator
+from keras.regularizers        import l2
+from keras.utils               import plot_model
 from operator                  import getitem
 
 
-def ljuxt(*fs):  # Kerasはジェネレーターを引数に取るのを嫌がるみたい、かつ、funcyはPython3だと積極的にジェネレーターを使うみたいなので、リストを返すjuxtを作りました。
-    return rcompose(juxt(*fs), list)
+def computational_graph(class_size):
+    # Utility functions.
 
+    def ljuxt(*fs):  # Kerasはジェネレーターを引数に取るのを嫌がるみたい、かつ、funcyはPython3だと積極的にジェネレーターを使うみたいなので、リストを返すjuxtを作りました。
+        return rcompose(juxt(*fs), list)
 
-def create_model(x, y):
-    def conv_2d(filters, kernel_size, strides=1):
-        return Conv2D(filters, kernel_size, strides=strides, padding='same', kernel_initializer='he_normal')
+    def batch_normalization():
+        return BatchNormalization()
 
-    def max_pooling_2d():
-        return MaxPooling2D(pool_size=3, strides=2, padding='same')
+    def relu():
+        return Activation('relu')
 
-    def bn_relu():
-        return rcompose(BatchNormalization(), Activation('relu'))
+    def conv(filters, kernel_size):
+        return Conv2D(filters, kernel_size, padding='same', kernel_initializer='he_normal', kernel_regularizer=l2(0.0005))
 
-    def bn_relu_conv(filters, kernel_size, strides=1):
-        return rcompose(bn_relu(), conv_2d(filters, kernel_size, strides))
+    def concatenate():
+        return Concatenate()
+
+    def add():
+        return Add()
+
+    def max_pooling():
+        return MaxPooling2D()
+
+    def dropout():
+        return Dropout(0.5)
+
+    def global_average_pooling():
+        return GlobalAveragePooling2D()
+
+    def dense(units, activation):
+        return Dense(units, activation=activation, kernel_regularizer=l2(0.0005))
+
+    # Define SqueezeNet
 
     def fire_module(filters_squeeze, filters_expand):
-        return rcompose(bn_relu_conv(filters_squeeze, 1),
-                        bn_relu(),
-                        ljuxt(conv_2d(filters_expand // 2, 1),
-                              conv_2d(filters_expand // 2, 3)),
-                        Concatenate())
+        return rcompose(batch_normalization(),
+                        conv(filters_squeeze, 1),
+                        batch_normalization(),
+                        relu(),
+                        ljuxt(rcompose(conv(filters_expand // 2, 1),
+                                       batch_normalization()),
+                              rcompose(conv(filters_expand // 2, 3),
+                                       batch_normalization())),
+                        concatenate())
 
     def fire_module_with_shortcut(filters_squeeze, filters_expand):
         return rcompose(ljuxt(fire_module(filters_squeeze, filters_expand),
                               identity),
-                        Add())
+                        add())
 
-    computational_graph = rcompose(conv_2d(96, 3),
-                                   # max_pooling_2d(),
-                                   fire_module(16, 128),
-                                   fire_module_with_shortcut(16, 128),
-                                   fire_module(32, 256),
-                                   max_pooling_2d(),
-                                   fire_module_with_shortcut(32, 256),
-                                   fire_module(48, 384),
-                                   fire_module_with_shortcut(48, 384),
-                                   fire_module(64, 512),
-                                   max_pooling_2d(),
-                                   fire_module_with_shortcut(64, 512),
-                                   # Dropout(0.5),
-                                   bn_relu_conv(y.shape[1], 1),
-                                   BatchNormalization(),
-                                   Activation('relu'),
-                                   GlobalAveragePooling2D(),
-                                   Activation('softmax'))
-
-    return Model(*juxt(identity, computational_graph)(Input(shape=x.shape[1:])))
+    return rcompose(conv(96, 3),
+                    # max_pooling(),
+                    fire_module(16, 128),
+                    fire_module_with_shortcut(16, 128),
+                    fire_module(32, 256),
+                    max_pooling(),
+                    fire_module_with_shortcut(32, 256),
+                    fire_module(48, 384),
+                    fire_module_with_shortcut(48, 384),
+                    fire_module(64, 512),
+                    max_pooling(),
+                    fire_module_with_shortcut(64, 512),
+                    dropout(),
+                    batch_normalization(),
+                    relu(),
+                    global_average_pooling(),
+                    dense(class_size, 'softmax'))
 
 
 def main():
     (x_train, y_train), (x_validation, y_validation) = load_data()
 
-    model = create_model(x_train, y_train)
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=SGD(decay=0.0001, momentum=0.9, nesterov=True),
-                  metrics=['accuracy'])
+    model = Model(*juxt(identity, computational_graph(y_train.shape[1]))(Input(shape=x_train.shape[1:])))
+    model.compile(loss='categorical_crossentropy', optimizer=SGD(momentum=0.9), metrics=['accuracy'])
 
     model.summary()
+    plot_model(model, to_file='./results/model.png')
 
     train_data      = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True, width_shift_range=0.125, height_shift_range=0.125, horizontal_flip=True)
     validation_data = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True)
@@ -84,10 +113,9 @@ def main():
     results = model.fit_generator(train_data.flow(x_train, y_train, batch_size=batch_size),
                                   steps_per_epoch=x_train.shape[0] // batch_size,
                                   epochs=epochs,
-                                  callbacks=[LearningRateScheduler(partial(getitem, tuple(take(epochs, concat(repeat(0.01, 1), repeat(0.1, 79), repeat(0.01, 42), repeat(0.001))))))],
+                                  callbacks=[LearningRateScheduler(partial(getitem, tuple(take(epochs, concat(repeat(0.01, 1), repeat(0.1, 99), repeat(0.01, 50), repeat(0.001))))))],
                                   validation_data=validation_data.flow(x_validation, y_validation, batch_size=batch_size),
-                                  validation_steps=x_validation.shape[0] // batch_size,
-                                  workers=4)
+                                  validation_steps=x_validation.shape[0] // batch_size)
 
     with open('./results/history.pickle', 'wb') as f:
         pickle.dump(results.history, f)
